d0d3a7316838b11d33776abd3b48c9972b3fff60
Omkar Nerale <omkarnerale2003@gmail.com>
2025-10-13 23:49:16 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -2,6 +2,6 @@ public class sample {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("Welcome to DevOps"):
+        System.out.println("Welcome to DevOps");
     }
 }
